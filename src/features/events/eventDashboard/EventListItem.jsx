0dff2221eaa0fbacfd9f9809616272d9d7a9a84f
Khaled <khalednehad@gmail.com>
2020-10-10 22:53:11 +0400

    End of section 4

--- src/features/events/eventDashboard/EventListItem.jsx
+++ src/features/events/eventDashboard/EventListItem.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button, Icon, Item, List, Segment } from 'semantic-ui-react';
 import EventListAttendee from './EventListAttendee';
 
-export default function EventListItem({event}) {
+export default function EventListItem({event, selectEvent , deleteEvent}) {
     return (
         <Segment.Group>
             <Segment>
@@ -35,7 +35,18 @@ export default function EventListItem({event}) {
             </Segment>
             <Segment clearing>
                 <span>Description pf Event</span>
-                <Button color='teal' floated='right' content='View' />
+                <Button
+                    onClick={() => selectEvent(event)}
+                    color='teal'
+                    floated='right'
+                    content='View'
+                />
+                <Button
+                    onClick={() => deleteEvent(event.id)}
+                    color='red'
+                    floated='right'
+                    content='Delete'
+                />
             </Segment>
         </Segment.Group>
 
---
+++ src/features/events/eventDetailed/EventDetailedPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default EventDetailedPage() {
+    return (
+        <h1>Event Detailed Page</h1>
+    )
+}
