6a2e2d0f6b2ab9f5c021e2151a2bf58346fa7ffb
Khaled <khalednehad@gmail.com>
2020-10-16 13:56:53 +0400

    middle of section 6

--- src/features/nav/SignedOutMenu.jsx
+++ src/features/nav/SignedOutMenu.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import { Button, Menu } from 'semantic-ui-react';
+import React from "react";
+import { Button, Menu } from "semantic-ui-react";
 
-export default function SignedOutMenu({setAuthenticated}) {
-    return (
-        <Menu.Item position='right'>
-            <Button onClick={() => setAuthenticated(true)} basic inverted content='Login' />
-            <Button basic inverted content='Register' style={{ marginLeft: '0.5em' }} />
-        </Menu.Item>
-    )
+export default function SignedOutMenu({ setAuthenticated }) {
+  return (
+    <Menu.Item position="right">
+      <Button
+        onClick={() => setAuthenticated(true)}
+        basic
+        inverted
+        content="Login"
+      />
+      <Button
+        basic
+        inverted
+        content="Register"
+        style={{ marginLeft: "0.5em" }}
+      />
+    </Menu.Item>
+  );
 }
